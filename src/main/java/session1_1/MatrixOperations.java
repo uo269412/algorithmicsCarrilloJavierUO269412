44a10723448ba931951a25cae6ec33784286aa3c
uo269412 <uo269412@EII-LS01-02.aulasuo.uniovi.es>
2020-01-29 19:47:33 +0100

    Unfinished version for the matrix operations

--- src/main/java/session1_1/MatrixOperations.java
+++ src/main/java/session1_1/MatrixOperations.java
@@ -19,18 +19,38 @@ public class MatrixOperations {
 		}
 	}
 
+	/**
+	 * Creates a matrix using data of the file provided as parameter
+	 * 
+	 * @param fileName of the file we want to read the matrix from
+	 */
 	public MatrixOperations(String fileName) {
 
 	}
 
+	/**
+	 * Returns the matrix size
+	 * 
+	 * @return integer indicating the size of the matrix
+	 */
 	public int getSize() {
 		return matrix.length;
 	}
 
+	/**
+	 * Prints through the console all the matrix element
+	 */
 	public void write() {
 
 	}
 
+	/**
+	 * Computes the summation of all the elements of the matrix diagonal. This
+	 * implementation must iterate over all the matrix elements, but only sums
+	 * appropriate elements
+	 * 
+	 * @return integer sum of the diagonal
+	 */
 	public int sumDiagonal1() {
 		int result = 0;
 		for (int i = 0; i < matrix.length; i++) {
@@ -43,6 +63,12 @@ public class MatrixOperations {
 		return result;
 	}
 
+	/**
+	 * Computes the summation of all the elements of the matrix diagonal considering
+	 * the elements of the main diagonal
+	 * 
+	 * @return integer sum of the diagonal
+	 */
 	public int sumDiagonal2() {
 		int result = 0;
 		for (int i = 0; i < matrix.length; i++) {
@@ -51,23 +77,37 @@ public class MatrixOperations {
 		return result;
 	}
 
+	/**
+	 * Given a matrix with integer numbers between 1 and 4, this method iterates
+	 * through the matrix starting at position (i, j) according to the following
+	 * number meanings: 1 – move up; 2 – move right; 3 – move down; 4 – move left.
+	 * Traversed elements would be set to -1 value. The process will finish if it
+	 * goes beyond the limits of the matrix or an already traversed position is
+	 * reached
+	 * 
+	 * @param i starting number of the row for the method
+	 * @param j starting number of the column for the method
+	 */
 	public void travelPath(int i, int j) {
 		while (i >= 0 && i < matrix.length && j >= 0 && j > matrix.length && matrix[i][j] != -1) {
 			switch (matrix[i][j]) {
 			case 1:
 				travelPath(i, j - 1);
+				matrix[i][j] = -1;
 				break;
 			case 2:
 				travelPath(i + 1, j);
+				matrix[i][j] = -1;
 				break;
 			case 3:
 				travelPath(i, j + 1);
+				matrix[i][j] = -1;
 				break;
 			case 4:
 				travelPath(i - 1, j);
+				matrix[i][j] = -1;
 				break;
 			}
-			matrix[i][j] = -1;
 		}
 	}
 }
