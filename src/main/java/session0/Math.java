f6e45faaa03c925ad631cb810c56a4595de7c22b
uo269412 <uo269412@EII-LS01-02.aulasuo.uniovi.es>
2020-01-29 19:15:46 +0100

    Second commit to githyb

--- src/main/java/session0/Math.java
+++ src/main/java/session0/Math.java
@@ -2,4 +2,7 @@ package session0;
 
 public class Math {
 
+	public int sum(int a, int b) {
+		return a + b;
+	}
 }
--- src/main/java/session0/MathTest.java
+++ src/main/java/session0/MathTest.java
@@ -1,5 +1,20 @@
 package session0;
 
-public class MathTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class MathTest {
+	Math math;
+
+	@Before
+	public void initialize() {
+		math = new Math();
+	}
+
+	@Test
+	public void test2Plus3Equals5() {
+		assertEquals(5, math.sum(2, 3));
+	}
 }
