c4407d4bc6bdb5c35b41adf45a2249db9e2582f3
UO269412 <UO269412@EII-LS01-02.aulasuo.uniovi.es>
2020-02-26 19:48:18 +0100

    Session 3 start

---
+++ src/main/java/session3/gilito/Gilito2.java
@@ -0,0 +1,18 @@
+package session3.gilito;
+
+/**
+ * This class simulates the algorithm currently used by TIO GILITO and
+ * challenges us to improve it for free (the usurer does not plan to pay us
+ * anything)
+ */
+public class Gilito2 extends Gilito1 {
+
+	public Gilito2(int n) {
+		super(n);
+	}
+
+	@Override
+	public int calculate() {
+		return 0;
+	}
+}
