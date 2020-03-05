ce2a3bda7291a886a4f5acc132b2f5fb8593dc38
javie <javie@DESKTOP-GNVG7TV>
2020-03-05 14:28:29 +0100

    almost finished

--- src/main/java/session3/gilito/Gilito2.java
+++ src/main/java/session3/gilito/Gilito2.java
@@ -13,7 +13,7 @@ public class Gilito2 extends Gilito1 {
 
 	@Override
 	public int calculate() {
-		return calculateUnderstandable(0, (getCoinsLength() - 1) / 2, getCoinsLength() / 2, getCoinsLength() - 1);
+		return calculateRec(0, (getCoinsLength() - 1) / 2, getCoinsLength() / 2, getCoinsLength() - 1);
 	}
 
 //	private int calculate2(int leftMin, int leftMax, int rightMin, int rightMax) {
@@ -54,17 +54,7 @@ public class Gilito2 extends Gilito1 {
 //		return rightMin;
 //	}
 
-	/**
-	 * More legible code for myself
-	 * 
-	 * @param leftMin
-	 * @param leftMax
-	 * @param rightMin
-	 * @param rightMax
-	 * @return
-	 */
-	@SuppressWarnings("unused")
-	private int calculateUnderstandable(int leftMin, int leftMax, int rightMin, int rightMax) {
+	private int calculateRec(int leftMin, int leftMax, int rightMin, int rightMax) {
 		if (rightMax % 2 == 0) {
 			if (balance(rightMax - 1, rightMax - 1, rightMax, rightMax) == 3) {
 				rightMax--;
@@ -74,79 +64,129 @@ public class Gilito2 extends Gilito1 {
 			} else {
 				return rightMax - 1;
 			}
-			System.out.println("RIGHTMAX IS NOW: " + rightMax);
 		}
-		System.out.println("leftMin: " + leftMin);
-		System.out.println("leftMax: " + leftMax);
-		System.out.println("rightMin: " + rightMin);
-		System.out.println("RightMax: " + rightMax);
 		int res = balance(leftMin, leftMax, rightMin, rightMax);
-		System.out.println("the balance is:" + res);
 		if (res == 1) {
-			System.out.println("left");
 			if (leftMin == (leftMin + leftMax - 1) / 2) {
-				if (balance(leftMin, leftMin, leftMax, leftMax) == 2) {
+				switch (balance(leftMin, leftMin, leftMax, leftMax)) {
+				case 2:
 					return leftMax;
-				} else if ((balance(leftMin, leftMin, leftMax, leftMax) == 1)) {
+				case 1:
 					return leftMin;
 				}
 			}
-			if ((rightMax + 1) / 2 %2 != 0) {
-				return calculateUnderstandable(leftMin, (leftMax + leftMin) / 2, (leftMax + leftMin) / 2, leftMax);
-			} else {
-				System.out.println("eh");
-				return calculateUnderstandable(leftMin, (leftMax + leftMin - 1) / 2, (leftMax+1 + leftMin) / 2, leftMax);
-			}
+			return ((rightMax + 1) / 2 % 2 != 0)
+					? calculateRec(leftMin, (leftMax + leftMin) / 2, (leftMax + leftMin) / 2, leftMax)
+					: calculateRec(leftMin, (leftMax + leftMin - 1) / 2, (leftMax + 1 + leftMin) / 2, leftMax);
 
 		} else if (res == 2) {
-			System.out.println("right");
 			if (rightMin == (rightMin + rightMax - 1) / 2) {
-				if (balance(rightMin, rightMin, rightMax, rightMax) == 2) {
+				switch (balance(rightMin, rightMin, rightMax, rightMax)) {
+				case 2:
 					return rightMax;
-				} else if ((balance(rightMin, rightMin, rightMax, rightMax) == 1)) {
+				case 1:
 					return rightMin;
 				}
 			}
-			if ((rightMax + 1) / 2 %2!= 0) {
-				return calculateUnderstandable(rightMin, (rightMin + rightMax) / 2, (rightMin + rightMax) / 2, rightMax);
-
-			} else {
-				return calculateUnderstandable(rightMin, (rightMin + rightMax - 1) / 2, (rightMin+1 + rightMax) / 2, rightMax);
-			}
-
+			return ((rightMax + 1) / 2 % 2 != 0)
+					? calculateRec(rightMin, (rightMin + rightMax) / 2, (rightMin + rightMax) / 2, rightMax)
+					: calculateRec(rightMin, (rightMin + rightMax - 1) / 2, (rightMin + 1 + rightMax) / 2, rightMax);
 		}
 		return rightMin;
 	}
 
+//	/**
+//	 * More legible code for myself
+//	 * 
+//	 * @param leftMin
+//	 * @param leftMax
+//	 * @param rightMin
+//	 * @param rightMax
+//	 * @return
+//	 */
+//	@SuppressWarnings("unused")
+//	private int calculateUnderstandable(int leftMin, int leftMax, int rightMin, int rightMax) {
+//		if (rightMax % 2 == 0) {
+//			if (balance(rightMax - 1, rightMax - 1, rightMax, rightMax) == 3) {
+//				rightMax--;
+//				leftMax--;
+//			} else if (balance(rightMax - 1, rightMax - 1, rightMax, rightMax) == 2) {
+//				return rightMax;
+//			} else {
+//				return rightMax - 1;
+//			}
+//			System.out.println("RIGHTMAX IS NOW: " + rightMax);
+//		}
+//		System.out.println("leftMin: " + leftMin);
+//		System.out.println("leftMax: " + leftMax);
+//		System.out.println("rightMin: " + rightMin);
+//		System.out.println("RightMax: " + rightMax);
+//		int res = balance(leftMin, leftMax, rightMin, rightMax);
+//		System.out.println("the balance is:" + res);
+//		if (res == 1) {
+//			System.out.println("left");
+//			if (leftMin == (leftMin + leftMax - 1) / 2) {
+//				if (balance(leftMin, leftMin, leftMax, leftMax) == 2) {
+//					return leftMax;
+//				} else if ((balance(leftMin, leftMin, leftMax, leftMax) == 1)) {
+//					return leftMin;
+//				}
+//			}
+//			if ((rightMax + 1) / 2 %2 != 0) {
+//				return calculateUnderstandable(leftMin, (leftMax + leftMin) / 2, (leftMax + leftMin) / 2, leftMax);
+//			} else {
+//				return calculateUnderstandable(leftMin, (leftMax + leftMin - 1) / 2, (leftMax+1 + leftMin) / 2, leftMax);
+//			}
+//
+//		} else if (res == 2) {
+//			System.out.println("right");
+//			if (rightMin == (rightMin + rightMax - 1) / 2) {
+//				if (balance(rightMin, rightMin, rightMax, rightMax) == 2) {
+//					return rightMax;
+//				} else if ((balance(rightMin, rightMin, rightMax, rightMax) == 1)) {
+//					return rightMin;
+//				}
+//			}
+//			if ((rightMax + 1) / 2 %2!= 0) {
+//				return calculateUnderstandable(rightMin, (rightMin + rightMax) / 2, (rightMin + rightMax) / 2, rightMax);
+//
+//			} else {
+//				return calculateUnderstandable(rightMin, (rightMin + rightMax - 1) / 2, (rightMin+1 + rightMax) / 2, rightMax);
+//			}
+//
+//		}
+//		return rightMin;
+//	}
+
 	public static void main(String arg[]) {
-		int n = Integer.parseInt(arg[0]); // number of coins (size of the problem)
-		Gilito2 gilito = new Gilito2(n);
-		// let's simulate the n possible cases - false currency in each position
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++)
-				gilito.setCoinWeight(j, REAL_WEIGHT); // authentic coin weight
-			gilito.setCoinWeight(i, FAKE_WEIGHT); // fake coin weight
-			gilito.calculate();
-		}
-		System.out.println("COINS=" + n + " ***AVERAGE ENERGY=" + gilito.getUsedWatts() / n + " watts");
+//		int n = Integer.parseInt(arg[0]); // number of coins (size of the problem)
+//		Gilito2 gilito = new Gilito2(n);
+//		// let's simulate the n possible cases - false currency in each position
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < n; j++)
+//				gilito.setCoinWeight(j, REAL_WEIGHT); // authentic coin weight
+//			gilito.setCoinWeight(i, FAKE_WEIGHT); // fake coin weight
+//			gilito.calculate();
+//		}
+//		System.out.println("COINS=" + n + " ***AVERAGE ENERGY=" + gilito.getUsedWatts() / n + " watts");
 //		   Gilito2 gilito2 = new Gilito2(4);
-//		   gilito2.setCoinWeight(0, FAKE_WEIGHT); 
+//		   gilito2.setCoinWeight(0, REAL_WEIGHT); 
 //		   gilito2.setCoinWeight(1, REAL_WEIGHT); 
 //		   gilito2.setCoinWeight(2, REAL_WEIGHT); 
-//		   gilito2.setCoinWeight(3, REAL_WEIGHT); 
+//		   gilito2.setCoinWeight(3, FAKE_WEIGHT); 
 //		   System.out.println("RESULT: " + gilito2.calculate());
 
-//		Gilito2 gilito2 = new Gilito2(9);
-//		gilito2.setCoinWeight(0, REAL_WEIGHT);
-//		gilito2.setCoinWeight(1, REAL_WEIGHT);
-//		gilito2.setCoinWeight(2, REAL_WEIGHT);
-//		gilito2.setCoinWeight(3, REAL_WEIGHT);
-//		gilito2.setCoinWeight(4, REAL_WEIGHT);
-//		gilito2.setCoinWeight(5, FAKE_WEIGHT);
-//		gilito2.setCoinWeight(6, REAL_WEIGHT);
-//		gilito2.setCoinWeight(7, REAL_WEIGHT);
-//		gilito2.setCoinWeight(8, REAL_WEIGHT);
-//		System.out.println("RESULT: " + gilito2.calculate());
+		Gilito2 gilito2 = new Gilito2(9);
+		gilito2.setCoinWeight(0, REAL_WEIGHT);
+		gilito2.setCoinWeight(1, REAL_WEIGHT);
+		gilito2.setCoinWeight(2, REAL_WEIGHT);
+		gilito2.setCoinWeight(3, REAL_WEIGHT);
+		gilito2.setCoinWeight(4, REAL_WEIGHT);
+		gilito2.setCoinWeight(5, REAL_WEIGHT);
+		gilito2.setCoinWeight(6, REAL_WEIGHT);
+		gilito2.setCoinWeight(7, REAL_WEIGHT);
+		gilito2.setCoinWeight(8, FAKE_WEIGHT);
+		System.out.println("RESULT: " + gilito2.calculate());
 
 	}
 }
